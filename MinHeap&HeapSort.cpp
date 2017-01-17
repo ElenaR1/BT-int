b4dfdadd036900b5a60fb26e2075f11a01b6c518
ElenaR1 <elena.radoycheva@abv.bg>
2017-01-17 21:07:50 +0200

    Update and rename MinHeap.cpp to MinHeap&HeapSort.cpp

--- MinHeap&HeapSort.cpp
+++ MinHeap&HeapSort.cpp
@@ -170,3 +170,79 @@ int main()
 
 	return 0;
 }
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void swap(int &a, int&b)
+{
+	int x;
+	x = a;
+	a = b;
+	b = x;
+}
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+void heapify(int arr[], int n, int i)
+{
+	int largest = i;  // Initialize largest as root
+	int l = 2 * i + 1;  // left = 2*i + 1
+	int r = 2 * i + 2;  // right = 2*i + 2
+
+						// If left child is larger than root
+	if (l < n && arr[l] > arr[largest])//za samllest obruhstam znacite tuk
+		largest = l;
+
+	// If right child is larger than largest so far
+	if (r < n && arr[r] > arr[largest])//i tuk
+		largest = r;
+
+	// If largest is not root
+	if (largest != i)
+	{
+		swap(arr[i], arr[largest]);
+
+		// Recursively heapify the affected sub-tree
+		heapify(arr, n, largest);
+	}
+
+
+	/*ili kato v tetradkata bez rekursivnoto izvikvzne gore: else {
+	return;
+	}*/
+}
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+	// Build heap (rearrange array)
+	//(n-1)/2 1viqt element koito e nalednik
+	for (int i = (n - 1) / 2; i >= 0; i--)
+		heapify(arr, n, i);
+
+	// One by one extract an element from heap
+	for (int i = n - 1; i >= 0; i--)
+	{
+		// Move current root to end
+		swap(arr[0], arr[i]);
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
+	}
+}
+
+int main() {
+
+	int a[7] = { 4,10,3,5,1,8,7 };
+	heapSort(a, 7);
+
+	for (size_t i = 0; i < 7; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
+
