ee1e524094d1c032faba9f9d83ca3e7314d13721
ElenaR1 <elena.radoycheva@abv.bg>
2017-01-18 11:01:19 +0200

    Update Data-Str tasks.cpp

--- Data-Str tasks.cpp
+++ Data-Str tasks.cpp
@@ -112,3 +112,254 @@ int main()
 	q.print();
 	return 0;
 }
+
+	
+	
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <assert.h>
+
+
+using namespace std;
+template <typename T>
+using mapFn = T(*)(const T&);
+
+template <typename T>
+struct node
+{
+	T data;
+	node<T>* left;
+	node<T>* right;
+	node():left(nullptr),right(nullptr){}
+	node(const T d, node<T> *l, node<T> *r) :data(d), left(l), right(r) {}
+};
+template <typename T>
+class tree
+{
+private:
+	node<T>* root;
+	bool add(T x, char*trace,node<T>* &subTreeRoot)
+	{
+		if (subTreeRoot == NULL)
+		{
+			if (strlen(trace) == 0)
+			{
+				subTreeRoot = new node<T>(x, NULL, NULL);
+				return true;
+			}
+		}
+		if (strlen(trace) > 0)
+		{
+			if (trace[0] == 'L')
+			{
+				return add(x, trace + 1, subTreeRoot->left);
+			}
+			if (trace[0] == 'R')
+			{
+				return add(x, trace + 1, subTreeRoot->right);
+			}
+		}
+	}
+	void print(node<T>* subTreeRoot)
+	{
+		if (subTreeRoot == NULL) return;
+		else
+		{
+			cout << subTreeRoot->data << " ";
+			print(subTreeRoot->left);
+			print(subTreeRoot->right);
+		}
+	}
+	void prettyPrintPrivate(node<T>*subTreeRoot, int curLevel)
+	{
+		if (subTreeRoot == NULL)
+		{
+			return;// kakvo pravi??
+		}
+		else
+		{
+			if (subTreeRoot->right != NULL)
+			{
+				prettyPrintPrivate(subTreeRoot->right, curLevel + 4);
+			}
+			if (curLevel != 0) {
+				cout << setw(curLevel) << ' ';
+			}
+			if (subTreeRoot->right != NULL)
+			{
+				cout << " /\n" << setw(curLevel) << ' ';
+			}
+			cout << subTreeRoot->data << "\n ";
+			if (subTreeRoot->left != NULL)
+			{
+				cout << setw(curLevel) << ' ' << " \\\n";// kakvo e 
+				prettyPrintPrivate(subTreeRoot->left, curLevel + 4);
+			}
+		}
+	}
+	bool member(const T& x,node<T>* subTreeRoot )
+	{
+		if (subTreeRoot == NULL) return false;
+		else
+		{
+			if (subTreeRoot->data == x) return true;
+			else
+			{
+				return member(x, subTreeRoot->left) || member(x, subTreeRoot->right);
+			}
+		}
+	}
+	void deleteAll(node<T>*subTreeRoot)
+	{
+		if (subTreeRoot == NULL) return;
+		else
+		{
+			deleteAll(subTreeRoot->left);
+			deleteAll(subTreeRoot->right);
+			delete subTreeRoot;
+		}
+	}
+	void map(mapFn<T> f, node<T>*subTreeRoot)
+	{
+		if (subTreeRoot == NULL) return;
+		else
+		{
+			subTreeRoot->data = f(subTreeRoot->data);
+			map(f, subTreeRoot->left);
+			map(f, subTreeRoot->right);
+		}
+	}
+	int max(int a, int b)
+	{
+		if (a > b)
+		{
+			return a;
+		}
+		else
+		{
+			return b;
+		}
+	}
+	int findHeightPrivate(node<T>*subTreeRoot)//height=max depth
+	{
+		if (subTreeRoot == NULL)
+		{
+			return 0;
+		}
+		else
+		{
+			return max(findHeightPrivate(subTreeRoot->left), findHeightPrivate(subTreeRoot->right)) + 1;
+		}
+	}
+	void longestSequence(node<T>* subTreeRoot,T expected, int curLen, int &len)
+	{
+		if (subTreeRoot == NULL) return;
+		
+		if (subTreeRoot->data == expected)
+			curLen ++;
+		else
+			curLen = 1;
+
+		len = max(len, curLen);
+		
+			cout << len << " ";
+		if(subTreeRoot->right)
+		longestSequence(subTreeRoot->right, subTreeRoot->data + 1, curLen, len);
+		
+		if(subTreeRoot->left)
+		longestSequence(subTreeRoot->left, subTreeRoot->data + 1, curLen, len);
+	}
+public:
+	tree() :root(nullptr){}
+	bool add(T x, char*trace)
+	{
+		return add(x, trace, root);
+	}
+	void print()
+	{
+		print(root);
+	}
+	void prettyPrint()
+	{
+		prettyPrintPrivate(root, 1);
+	}
+	bool member(const T& x)
+	{
+		return member(x,root);
+	}
+	~tree()
+	{
+		deleteAll(root);
+	}
+	void map(mapFn<T> f)
+	{
+		map(f, root);
+	}
+	int findHeight()
+	{
+		return findHeightPrivate(root);
+	}
+	//Find longest consecutive sequence in binary tree.
+	int longestSequence()
+	{
+		int len = 0;
+		longestSequence(root,root->data, 0,len);
+		return len;
+	}
+	//2. Construct a binary tree from given postorder and inorder traversals.
+	node<T>* buildTree(int*in, int*post, int start, int end, int index)
+	{
+		if (start > end) return NULL;
+		node<T>* newNode = new node<T>(post[index], NULL, NULL);//vzima posledniq element na postorder-a
+			index--;
+		//if this node has no children
+		if (start == end)
+			return NULL;
+		/* Else find the index of this node in Inorder
+		traversal */
+		int itindex = search(in, start, end, newNode->data);
+
+		newNode->right = buildTree(in, post, itindex+1, end, index);
+		newNode->right = buildTree(in, post, start, itindex-1, index );
+	}
+	node<T>* buildTree(int*in,int*post,int n)
+	{
+		int index = n - 1;
+		return buildTree(in, post, 0, n - 1, index);
+		
+	}
+	/* Function to find index of value in arr[start...end]
+	The function assumes that value is postsent in in[] */
+	int search(int arr[], int strt, int end, int value)
+	{
+		int i;
+		for (i = strt; i <= end; i++)
+		{
+			if (arr[i] == value)
+				break;
+		}
+		return i;
+	}
+
+};
+int main() {
+	tree<int> t;
+	t.add(6,"");
+	t.add(9, "R");
+	t.add(10, "RR");
+	t.add(11, "RRR");
+	t.add(7, "L");
+	t.prettyPrint();
+	cout << endl;
+	cout << t.longestSequence();
+
+	int inOrder[] = { 4,2,5,1,6,7,3,8 };
+	int postOrder[] = { 4,5,2,6,7,8,3,1 };
+	tree<int> ord;
+	ord.buildTree(inOrder, postOrder, 8);
+	
+	return 0;
+}
